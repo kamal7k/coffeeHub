715836da97772ef5f4b3aa532f471816cfb6afc2
kamal7k <kunwarkamal7k@gmail.com>
2024-12-30 00:49:58 +0545

    update v0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
+export default withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       backgroundColor: {
-        'rd': '#3F3D56'
+        rd: "#3F3D56",
       },
       animation: {
-        'bounce-slow': 'bounce 2s linear infinite',
+        "bounce-slow": "bounce 2s linear infinite",
       },
     },
     screens: {
-      '2xl': { 'max': '1536px' },
-      'xl': { 'max': '1280px' },
-      'lg': { 'max': '1024px' },
-      'md': { 'max': '768px' },
-      'sm': { 'max': '640px' },
+      "2xl": { max: "1536px" },
+      xl: { max: "1280px" },
+      lg: { max: "1024px" },
+      md: { max: "768px" },
+      sm: { max: "640px" },
     },
   },
   plugins: [],
-})
-
+});
